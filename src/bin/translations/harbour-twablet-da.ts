b6455803e8802a59758070db9aa59ee042150374
Ole Holm Frandsen <froksen@gmail.com>
2015-10-31 16:33:11 +0100

    Added danish translation
    Translated the program into danish.

---
+++ src/bin/translations/harbour-twablet-da.ts
@@ -0,0 +1,313 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="da_DK" sourcelanguage="en">
+<context>
+    <name>AccountPage</name>
+    <message>
+        <source>Home</source>
+        <translation>Hjem</translation>
+    </message>
+    <message>
+        <source>Mentions</source>
+        <translation>Meddeleser</translation>
+    </message>
+    <message>
+        <source>Add account</source>
+        <translation>Tilføj konto</translation>
+    </message>
+    <message>
+        <source>Name of the account</source>
+        <translation>Navn på konto</translation>
+    </message>
+    <message>
+        <source>Add home timeline</source>
+        <translation>Tilføj tidslinje hjem</translation>
+    </message>
+    <message>
+        <source>Add mentions timeline</source>
+        <translation>Tilføj tidslinje meddeleser</translation>
+    </message>
+</context>
+<context>
+    <name>AccountsPage</name>
+    <message>
+        <source>Accounts</source>
+        <translation>Konti</translation>
+    </message>
+    <message>
+        <source>Removing account</source>
+        <translation>Fjerner konto</translation>
+    </message>
+    <message>
+        <source>Remove</source>
+        <translation>Fjern</translation>
+    </message>
+    <message>
+        <source>There is no account defined. Pull down to add one account</source>
+        <translation>Der er ingen konto defineret. Træk ned for at tilføje en</translation>
+    </message>
+    <message>
+        <source>Add account</source>
+        <translation>Tilføj konto</translation>
+    </message>
+</context>
+<context>
+    <name>AddColumnPage</name>
+    <message>
+        <source>Add tab</source>
+        <translation>Tilføj fane</translation>
+    </message>
+    <message>
+        <source>Name of the column</source>
+        <translation>Navn på kolonne</translation>
+    </message>
+    <message>
+        <source>Column type</source>
+        <translation>kolonnetype</translation>
+    </message>
+    <message>
+        <source>Account</source>
+        <translation>Konto</translation>
+    </message>
+    <message>
+        <source>Search query</source>
+        <translation>Søgeforespørgsel</translation>
+    </message>
+    <message>
+        <source>Search result type</source>
+        <translation>Søgeresultattype</translation>
+    </message>
+    <message>
+        <source>Recent</source>
+        <translation>Seneste</translation>
+    </message>
+    <message>
+        <source>Popular</source>
+        <translation>Populære</translation>
+    </message>
+    <message>
+        <source>Mixed</source>
+        <translation>Blandede</translation>
+    </message>
+</context>
+<context>
+    <name>AuthentificationPage</name>
+    <message>
+        <source>Retry</source>
+        <translation>Prøv igen</translation>
+    </message>
+    <message>
+        <source>Add account</source>
+        <translation>Tilføj konto</translation>
+    </message>
+    <message>
+        <source>Log into Twitter using the web page and enter the PIN code below</source>
+        <translation>Log på Twitter ved at bruge hjemmesiden og skrive PIN-koden herunder</translation>
+    </message>
+    <message>
+        <source>Enter PIN code here</source>
+        <translation>Skriv PIN-kode her</translation>
+    </message>
+    <message>
+        <source>Log in</source>
+        <translation>Log ind</translation>
+    </message>
+</context>
+<context>
+    <name>ColumnLayout</name>
+    <message>
+        <source>Removing column</source>
+        <translation>Fjerner kolonne</translation>
+    </message>
+    <message>
+        <source>No tweets</source>
+        <translation>Ingen tweets</translation>
+    </message>
+</context>
+<context>
+    <name>CoverPage</name>
+    <message numerus="yes">
+        <source>%1: %n</source>
+        <translation>
+            <numerusform></numerusform>
+        </translation>
+    </message>
+</context>
+<context>
+    <name>LoadMoreButton</name>
+    <message>
+        <source>Load more</source>
+        <translation>Indlæs flere</translation>
+    </message>
+</context>
+<context>
+    <name>MainPage</name>
+    <message>
+        <source>Accounts</source>
+        <translation>Konto</translation>
+    </message>
+    <message>
+        <source>Add column</source>
+        <translation>Tilføj kolonne</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation>Om</translation>
+    </message>
+    <message>
+        <source>Refresh</source>
+        <translation>Genindlæs</translation>
+    </message>
+</context>
+<context>
+    <name>RightPanel</name>
+    <message>
+        <source>%1&apos;s favorites</source>
+        <translation>%1s favoritter</translation>
+    </message>
+    <message>
+        <source>Tweets from %1</source>
+        <translation>Tweet fra %1</translation>
+    </message>
+    <message>
+        <source>%1&apos;s friends</source>
+        <translation>%1s venner</translation>
+    </message>
+    <message>
+        <source>%1&apos;s followers</source>
+        <translation>%1s følgere</translation>
+    </message>
+</context>
+<context>
+    <name>StatusPlaceholder</name>
+    <message>
+        <source>Retry</source>
+        <translation>Prøv igen</translation>
+    </message>
+</context>
+<context>
+    <name>StatusUpdater</name>
+    <message>
+        <source>Reply to %1</source>
+        <translation>Svar til %1</translation>
+    </message>
+    <message>
+        <source>Tweet something</source>
+        <translation>Tweet noget</translation>
+    </message>
+    <message numerus="yes">
+        <source>%n left</source>
+        <translation>
+            <numerusform>%n tilbage</numerusform>
+            <numerusform>%n tilbage</numerusform>
+        </translation>
+    </message>
+    <message>
+        <source>Tweet too long</source>
+        <translation>Tweetet er for langt</translation>
+    </message>
+    <message>
+        <source>Reply</source>
+        <translation>Prøv igen</translation>
+    </message>
+    <message>
+        <source>Send</source>
+        <translation>Send</translation>
+    </message>
+    <message>
+        <source>Account: %1</source>
+        <translation>Konto: %1</translation>
+    </message>
+</context>
+<context>
+    <name>TweetDelegate</name>
+    <message>
+        <source>Retweeted by %1</source>
+        <translation>Retweeted af %1</translation>
+    </message>
+</context>
+<context>
+    <name>TweetPage</name>
+    <message>
+        <source>Tweet</source>
+        <translation>Tweet</translation>
+    </message>
+    <message>
+        <source>Open in browser</source>
+        <translation>Åben i browser</translation>
+    </message>
+</context>
+<context>
+    <name>TweetsPage</name>
+    <message>
+        <source>Add as column</source>
+        <translation>Tilføj som kolonne</translation>
+    </message>
+    <message>
+        <source>Refresh</source>
+        <translation>Genindlæs</translation>
+    </message>
+</context>
+<context>
+    <name>UserPage</name>
+    <message numerus="yes">
+        <source>%n
+tweets</source>
+        <translation>
+            <numerusform></numerusform>
+        </translation>
+    </message>
+    <message numerus="yes">
+        <source>%n
+friends</source>
+        <translation>
+            <numerusform>%n
+ven</numerusform>
+            <numerusform>%n
+venner</numerusform>
+        </translation>
+    </message>
+    <message numerus="yes">
+        <source>%n
+followers</source>
+        <translation>
+            <numerusform>%n
+følger</numerusform>
+            <numerusform>%n
+følgere</numerusform>
+        </translation>
+    </message>
+    <message numerus="yes">
+        <source>%n tweets per day</source>
+        <translation>
+            <numerusform>%n tweet pr dag</numerusform>
+            <numerusform>%n tweets pr dag</numerusform>
+        </translation>
+    </message>
+    <message numerus="yes">
+        <source>%n favourites</source>
+        <translation>
+            <numerusform>%n favorit</numerusform>
+            <numerusform>%n favoritter</numerusform>
+        </translation>
+    </message>
+    <message>
+        <source>Subscribed lists</source>
+        <translation>Abonnerede lister</translation>
+    </message>
+    <message numerus="yes">
+        <source>Listed in %n lists</source>
+        <translation>
+            <numerusform>Opført i %n liste</numerusform>
+            <numerusform>Opført i %n lister</numerusform>
+        </translation>
+    </message>
+</context>
+<context>
+    <name>UsersPage</name>
+    <message>
+        <source>No friends</source>
+        <translation>Ingen venner</translation>
+    </message>
+</context>
+</TS>
